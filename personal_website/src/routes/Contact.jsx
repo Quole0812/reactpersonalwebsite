fc30f37496cd3c068c4dafcd4d9262c2ab6614bb
Jackson <wnj9tx@virginia.edu>
2025-05-19 15:23:42 -0400

    about page

--- personal_website/src/routes/Contact.jsx
+++ personal_website/src/routes/Contact.jsx
@@ -1,10 +1,29 @@
 import Header from "../components/Header";
-import "../Style.css";
 export default function Contact() {
   return (
     <>
     <Header />
-      <p>This is the Contact page</p>
+      <div className="contact-container">
+        <h2>Contact Me</h2>
+        <form className="contact-form">
+          <label>
+            Name:
+            <input type="text" name="name"   />
+          </label>
+
+          <label>
+            Email:
+            <input type="email" name="email"  />
+          </label>
+
+          <label>
+            Message:
+            <textarea name="message" rows="5"  ></textarea>
+          </label>
+
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </>
   );
 }
--- personal_website/src/routes/Projects.jsx
+++ personal_website/src/routes/Projects.jsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header";
-import "../Style.css";
 export default function Projects() {
   return (
     <>
