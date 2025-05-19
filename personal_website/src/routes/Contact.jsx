c3f2cbdbbc247c6f8475ea33d2055dcf0d7ec14d
Jackson <wnj9tx@virginia.edu>
2025-05-19 15:14:26 -0400

    header style

--- personal_website/src/routes/Contact.jsx
+++ personal_website/src/routes/Contact.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import "../Style.css";
 export default function Contact() {
   return (
     <>
--- personal_website/src/routes/Home.jsx
+++ personal_website/src/routes/Home.jsx
@@ -6,10 +6,10 @@ export default function Home() {
     <Header />
       <div className="homecontainer">
         <h1 className="hometitle">Hi, I'm Jackson Le</h1>
-        <p>
+        <p className="subtitlehome">
             Computer Science student at UVA passionate about building smart and scalable tech.
         </p>
-        <p>Browse around to learn more about background, projects, and interests.</p>
+        <p className="subtitlehome">Browse around to learn more about background, projects, and interests.</p>
       </div>
     </>
   );
